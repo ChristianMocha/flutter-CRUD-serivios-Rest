1a66be52f503ec10448c32ff32d48a0e0902dd20
christianmocha <letmy.4@hotmail.com>
2021-01-23 16:28:15 -0500

    cargar fotosgrafias a firebase y actualizar registro.

--- lib/src/pages/producto_page.dart
+++ lib/src/pages/producto_page.dart
@@ -130,16 +130,19 @@ class _ProductoPageState extends State<ProductoPage> {
     );
   }
 
-  void _submit(){
+  void _submit() async{
 
-    
+
 
     if(!formKey.currentState.validate()) return;
 
     formKey.currentState.save();
-    setState(() {
-    _guardando=true;
-    });
+
+    setState(() {_guardando=true;});
+
+    if(foto != null){
+      producto.fotoUrl =  await productoProvider.subirImagen(foto);
+    }
 
     if(producto.id == null){
     productoProvider.crearProducto(producto);
